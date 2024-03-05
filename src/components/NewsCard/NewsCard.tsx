df4a3bd1c56360a2a75a35ffd1de4a3a16f483f6
Dillona25 <Dillyb00by49@gmail.com>
2024-03-05 16:07:54 -0600

    fix - updated names

--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const NewsCard = (props: Props) => {
   return (
-    <div className="relative flex flex-col h-[440px] w-[288px] sm:w-[400px] sm:h-fit rounded-[10px] bg-white shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+    <div className="relative flex flex-col gap-4 h-[440px] w-[288px] rounded-[10px] bg-white shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
       {props.isLoggedIn ? (
         <div className="bg-white h-10 w-10 absolute right-[16px] top-[16px] rounded-[10px] flex">
           <button className="bg-saveIcon h-6 w-6 m-auto"></button>
@@ -17,17 +17,15 @@ export const NewsCard = (props: Props) => {
       <img
         src={Nature}
         alt="Nature Image"
-        className="h-[196px] rounded-t-[10px] sm:h-[272px]"
+        className="max-h-[196px] rounded-t-[10px]"
       />
-      <div className="px-4 py-5">
-        <h1 className="font-RobotoSlab font-normal mb-4 text-[22px] sm:text-[26px] leading-[24px] sm:leading-[30px]">
+      <div className="px-4">
+        <h1 className="font-RobotoSlab font-normal mb-4 text-[22px] leading-[24px]">
           Everyone Needs a Special 'Sit Spot'
         </h1>
-        <p className="font-normal h-[88px] mb-[30px] sm:text-[16px] sm:h-[110px] overflow-hidden text-ellipsis">
+        <p className="font-normal mb-[30px]">
           Ever since I read Richard Louv's influential book, "Last Child in the
-          Woods," the idea of having a special "sit spot" has stuck with me.
-          This advice, which Louv attributes to nature educator Jon Young, is
-          for both adults and children to find...
+          Woods," the idea of having a special "sit spot" has stuck with me.{" "}
         </p>
         <div className="flex gap-4">
           <p className="font-bold font-RobotoSlab text-[#84898b] ">
