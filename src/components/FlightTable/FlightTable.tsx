bcc1dd5c511e64f90f5b97cde92a757889fa1414
Dillona25 <Dillyb00by49@gmail.com>
2024-10-13 14:15:37 -0500

    Updated flight table

--- src/components/FlightTable/FlightTable.tsx
+++ src/components/FlightTable/FlightTable.tsx
@@ -1,3 +1,5 @@
+import { FaArrowCircleDown } from "react-icons/fa";
+
 interface FlightTableProps {
   departures: any[];
   arrivals: any[];
@@ -8,49 +10,88 @@ export const FlightTable: React.FC<FlightTableProps> = ({
   arrivals,
 }) => {
   return (
-    <section className="py-8 bg-[#f5f6f7] px-4">
-      <div className="max-w-[1600px] m-auto flex flex-wrap justify-center gap-4">
+    <section className="py-8 bg-[#f5f6f7] px-4 scroll-smooth">
+      <div className="max-w-[1600px] m-auto flex flex-col justify-center gap-4">
+        <div className="flex flex-col gap-2 m-auto justify-center">
+          <h1 className="font-bold text-base">Quick Scroll To Arrivals</h1>
+          <a
+            href="#arrivals"
+            className="bg-gray-200 rounded-full p-4 w-fit m-auto"
+          >
+            <FaArrowCircleDown className="text-2xl hover:scale-110 ease-in-out duration-75 cursor-pointer" />
+          </a>
+        </div>
         {/* Departure Table */}
-        <table className="w-full sm:w-[48%] bg-white border border-gray-200 rounded-lg">
+        <table className="w-full bg-white border border-gray-200 rounded-lg mt-5">
           <thead>
             <tr>
               <th
                 className="py-2 px-4 border-b text-center bg-[#76a0e9] font-RobotoSlab"
-                colSpan={6}
+                colSpan={7}
               >
                 Departures
               </th>
             </tr>
-            <tr className="font-RobotoSlab text-base">
-              <th className="py-2 px-4 border-b">Time</th>
+            <tr className="font-RobotoSlab text-sm">
+              {" "}
+              {/* Adjusted to text-sm */}
+              <th className="py-2 px-4 border-b">Departure Time</th>
+              <th className="py-2 px-4 border-b hidden md:table-cell">
+                Status
+              </th>{" "}
+              {/* Hidden on sm and smaller */}
               <th className="py-2 px-4 border-b">Gate</th>
-              <th className="py-2 px-4 border-b">Airline</th>
+              <th className="py-2 px-4 border-b hidden md:table-cell">
+                Airline
+              </th>{" "}
+              {/* Hidden on sm and smaller */}
               <th className="py-2 px-4 border-b">Flight</th>
-              <th className="py-2 px-4 border-b">Origin</th>
+              <th className="py-2 px-4 border-b hidden md:table-cell">
+                Origin
+              </th>{" "}
+              {/* Hidden on sm and smaller */}
               <th className="py-2 px-4 border-b">Destination</th>
             </tr>
           </thead>
           <tbody>
             {departures.map((departure, index) => (
-              <tr className="text-center font-Roboto text-base" key={index}>
+              <tr className="text-center font-Roboto text-sm" key={index}>
+                {" "}
+                {/* Adjusted to text-sm */}
                 <td className="py-2 px-4 border-b">
                   {new Date(
                     departure.departure.actualTime ||
                       departure.departure.scheduledTime
-                  ).toLocaleTimeString()}
+                  ).toLocaleTimeString([], {
+                    hour: "numeric",
+                    minute: "2-digit",
+                    hour12: true,
+                  })}
+                </td>
+                <td
+                  className={`py-2 px-4 border-b hidden md:table-cell ${
+                    departure.status === "active" || "scheduled"
+                      ? `text-green-600`
+                      : `text-red-600`
+                  }`}
+                >
+                  {departure.status.charAt(0).toUpperCase() +
+                    departure.status.slice(1)}
                 </td>
                 <td className="py-2 px-4 border-b">
-                  {departure.departure.gate === "null"
-                    ? "N/A"
-                    : departure.departure.gate}
+                  {departure.departure.gate ?? "N/A"}
                 </td>
-                <td className="py-2 px-4 border-b">{departure.airline.name}</td>
+                <td className="py-2 px-4 border-b hidden md:table-cell">
+                  {departure.airline.name}
+                </td>{" "}
+                {/* Hidden on sm and smaller */}
                 <td className="py-2 px-4 border-b">
-                  {departure.flight.number}
+                  {departure.flight.icaoNumber}
                 </td>
-                <td className="py-2 px-4 border-b">
+                <td className="py-2 px-4 border-b hidden md:table-cell">
                   {departure.departure.iataCode}
-                </td>
+                </td>{" "}
+                {/* Hidden on sm and smaller */}
                 <td className="py-2 px-4 border-b">
                   {departure.arrival.iataCode}
                 </td>
@@ -60,39 +101,78 @@ export const FlightTable: React.FC<FlightTableProps> = ({
         </table>
 
         {/* Arrival Table */}
-        <table className="w-full sm:w-[48%] bg-white border border-gray-200 rounded-lg">
+        <table
+          id="arrivals"
+          className="w-full bg-white border border-gray-200 rounded-lg"
+        >
           <thead>
             <tr>
               <th
                 className="py-2 px-4 border-b text-center bg-[#76a0e9] font-RobotoSlab"
-                colSpan={6}
+                colSpan={7}
               >
                 Arrivals
               </th>
             </tr>
-            <tr className="font-RobotoSlab text-base">
-              <th className="py-2 px-4 border-b">Time</th>
+            <tr className="font-RobotoSlab text-sm">
+              {" "}
+              {/* Adjusted to text-sm */}
+              <th className="py-2 px-4 border-b">Arrival Time</th>
+              <th className="py-2 px-4 border-b hidden md:table-cell">
+                Status
+              </th>{" "}
+              {/* Hidden on sm and smaller */}
               <th className="py-2 px-4 border-b">Gate</th>
-              <th className="py-2 px-4 border-b">Airline</th>
+              <th className="py-2 px-4 border-b hidden md:table-cell">
+                Airline
+              </th>{" "}
+              {/* Hidden on sm and smaller */}
               <th className="py-2 px-4 border-b">Flight</th>
-              <th className="py-2 px-4 border-b">Origin</th>
+              <th className="py-2 px-4 border-b hidden md:table-cell">
+                Origin
+              </th>{" "}
+              {/* Hidden on sm and smaller */}
               <th className="py-2 px-4 border-b">Destination</th>
             </tr>
           </thead>
           <tbody>
             {arrivals.map((arrival, index) => (
-              <tr className="text-center font-Roboto text-base" key={index}>
+              <tr className="text-center font-Roboto text-sm" key={index}>
+                {" "}
+                {/* Adjusted to text-sm */}
                 <td className="py-2 px-4 border-b">
                   {new Date(
                     arrival.arrival.actualTime || arrival.arrival.scheduledTime
-                  ).toLocaleTimeString()}
+                  ).toLocaleTimeString([], {
+                    hour: "numeric",
+                    minute: "2-digit",
+                    hour12: true,
+                  })}
                 </td>
-                <td className="py-2 px-4 border-b">{arrival.arrival.gate}</td>
-                <td className="py-2 px-4 border-b">{arrival.airline.name}</td>
-                <td className="py-2 px-4 border-b">{arrival.flight.number}</td>
+                <td
+                  className={`py-2 px-4 border-b hidden md:table-cell ${
+                    arrival.status === "landed" || "scheduled"
+                      ? `text-green-600`
+                      : `text-red-600`
+                  }`}
+                >
+                  {arrival.status.charAt(0).toUpperCase() +
+                    arrival.status.slice(1)}
+                </td>
                 <td className="py-2 px-4 border-b">
-                  {arrival.departure.iataCode}
+                  {arrival.arrival.gate ?? "N/A"}
                 </td>
+                <td className="py-2 px-4 border-b hidden md:table-cell">
+                  {arrival.airline.name}
+                </td>{" "}
+                {/* Hidden on sm and smaller */}
+                <td className="py-2 px-4 border-b">
+                  {arrival.flight.icaoNumber}
+                </td>
+                <td className="py-2 px-4 border-b hidden md:table-cell">
+                  {arrival.departure.iataCode}
+                </td>{" "}
+                {/* Hidden on sm and smaller */}
                 <td className="py-2 px-4 border-b">
                   {arrival.arrival.iataCode}
                 </td>
