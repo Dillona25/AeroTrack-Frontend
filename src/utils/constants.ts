f6e9becd4cc1cc919035d80f3b61faa06a378f62
Dillona25 <Dillyb00by49@gmail.com>
2024-10-22 19:03:44 -0500

    status commit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? `https://api.AeroTrack.twilightparadox.com`
+    ? `https://api.aerotrack.twilightparadox.com`
     : "http://localhost:3001";
