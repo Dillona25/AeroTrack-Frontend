a630c6255d43e54884d177764c651c2c83be32ab
Dillona25 <Dillyb00by49@gmail.com>
2025-01-09 22:29:16 -0600

    Updated modal animations

--- src/components/SignInModal/SignInModal.tsx
+++ src/components/SignInModal/SignInModal.tsx
@@ -2,7 +2,6 @@ import { Modal } from "../Modal/Modal";
 import { Form } from "../Form/Form";
 import { Button } from "../Button/Button";
 import { useForm, SubmitHandler } from "react-hook-form";
-import { motion } from "framer-motion";
 import { checkPasswords } from "../../utils/authApi";
 
 type Props = {
@@ -45,69 +44,63 @@ export const SignInModal = (props: Props) => {
   };
   return (
     <Modal>
-      <motion.div
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        transition={{ duration: 0.2 }}
-      >
-        <h1 className="font-normal sm:text-[30px]">Login</h1>
-        <button
-          onClick={props.closeModal}
-          className="bg-closeIcon h-6 w-6 absolute right-[15px] top-[15px]"
-        ></button>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <Form.TextInput
-            labelText="Email"
-            placeholder="Email"
-            register={register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /[\w\-.]+@([\w-]+\.)+[\w-]{2,4}/,
-                message: "Invalid Email",
-              },
-            })}
-            onChange={(evt) => {
-              const target = evt.target as HTMLInputElement;
-              setValue("email", target.value, { shouldValidate: true });
-            }}
-          />
-          {errors.email && <Form.ErrorMessage message={errors.email.message} />}
+      <h1 className="font-normal sm:text-[30px]">Login</h1>
+      <button
+        onClick={props.closeModal}
+        className="bg-closeIcon h-6 w-6 absolute right-[15px] top-[15px]"
+      ></button>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.TextInput
+          labelText="Email"
+          placeholder="Email"
+          register={register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /[\w\-.]+@([\w-]+\.)+[\w-]{2,4}/,
+              message: "Invalid Email",
+            },
+          })}
+          onChange={(evt) => {
+            const target = evt.target as HTMLInputElement;
+            setValue("email", target.value, { shouldValidate: true });
+          }}
+        />
+        {errors.email && <Form.ErrorMessage message={errors.email.message} />}
 
-          <Form.TextInput
-            type="password"
-            labelText="Password"
-            placeholder="Password"
-            register={register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 8,
-                message: "Use 8 or more characters",
-              },
-            })}
-            onChange={(evt) => {
-              const target = evt.target as HTMLInputElement;
-              setValue("password", target.value, { shouldValidate: true });
-            }}
-          />
-          {errors.password && (
-            <Form.ErrorMessage message={errors.password.message} />
-          )}
+        <Form.TextInput
+          type="password"
+          labelText="Password"
+          placeholder="Password"
+          register={register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Use 8 or more characters",
+            },
+          })}
+          onChange={(evt) => {
+            const target = evt.target as HTMLInputElement;
+            setValue("password", target.value, { shouldValidate: true });
+          }}
+        />
+        {errors.password && (
+          <Form.ErrorMessage message={errors.password.message} />
+        )}
 
-          <Button
-            text="Login"
-            className={`bg-black mt-3 ${
-              isValid ? "" : "opacity-50 cursor-not-allowed"
-            }`}
-            disabled={!isValid}
-          />
-        </Form>
-        <p className="text-[14px] font-normal text-center pt-4">
-          No Account? Signup{" "}
-          <button onClick={props.handleSignUpModal} className="underline">
-            here
-          </button>
-        </p>
-      </motion.div>
+        <Button
+          text="Login"
+          className={`bg-black mt-3 ${
+            isValid ? "" : "opacity-50 cursor-not-allowed"
+          }`}
+          disabled={!isValid}
+        />
+      </Form>
+      <p className="text-[14px] font-normal text-center pt-4">
+        No Account? Signup{" "}
+        <button onClick={props.handleSignUpModal} className="underline">
+          here
+        </button>
+      </p>
     </Modal>
   );
 };
